44dfdf4a5abd94bec30dd1c78d9b74499a14460a
Lindgard <a.b.lindgard@gmail.com>
2023-12-07 23:41:26 +0100

    added a blogpost-table to the database

--- backend/db/dbConfig.js
+++ backend/db/dbConfig.js
@@ -1,4 +1,4 @@
-const sqlite3 = require("sqlite3");
+const sqlite3 = require("sqlite3").verbose();
 
 let db = new sqlite3.Database("./database.db");
 
@@ -8,8 +8,19 @@ db.serialize(() => {
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT NOT NULL,
     email TEXT NOT NULL UNIQUE,
+    hashed_password TEXT NOT NULL,
     dateCreated DATETIME DEFAULT CURRENT_TIMESTAMP
     )
+  `);
+  db.run(`
+  CREATE TABLE IF NOT EXISTS posts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    content TEXT NOT NULL,
+    user_id INTEGER NOT NULL,
+    dateCreated DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+  )
   `);
 });
 
